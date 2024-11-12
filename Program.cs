901e4ac8770356c1e8dbba627259e5b338329828
Timur <tvplus333@yandex.ru>
2024-11-12 13:33:36 +0300

    Создали команду, которая просит пользователя ввести исходный массив

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Просим пользователя ввести исходный массив с помощью клавиатуры
+        Console.WriteLine("Пожалуйста, введите все элементы массива через пробел: ");
+        string input = Console.ReadLine();
+        string[] initialArray = input.Split(' ');
+
+        // Далее подсчитываем кол-во строк, в которых длина <= трем символам
+        int count = 0;
+        for (int i = 0; i < initialArray.Length; i++)
+        {
+            if (initialArray[i].Length <= 3)
+            {
+                count++
+            }
+        }
